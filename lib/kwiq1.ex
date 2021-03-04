64245f46252d50d932ea56af116dbe9b967140d7
Sandesh Soni <hello@sandeshsoni.com>
2021-03-04 14:58:21 +0530

    init blank project with sup

---
+++ lib/kwiq1.ex
@@ -0,0 +1,18 @@
+defmodule Kwiq1 do
+  @moduledoc """
+  Documentation for `Kwiq1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kwiq1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kwiq1_test.exs
@@ -0,0 +1,8 @@
+defmodule Kwiq1Test do
+  use ExUnit.Case
+  doctest Kwiq1
+
+  test "greets the world" do
+    assert Kwiq1.hello() == :world
+  end
+end
