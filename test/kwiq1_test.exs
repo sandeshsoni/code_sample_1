4f222561033d7a00621a7cb6f08d391060a1e327
Sandesh Soni <hello@sandeshsoni.com>
2021-03-04 15:24:13 +0530

    skip tests with tag skip

--- test/kwiq1_test.exs
+++ test/kwiq1_test.exs
@@ -4,16 +4,19 @@ defmodule Kwiq1Test do
 
   describe "responds to " do
 
+    @tag :skip
     test "hello functions 1 to 11" do
       assert Kwiq1.hello(1)
       assert Kwiq1.hello(2)
       assert Kwiq1.hello(9)
     end
 
+    @tag :skip
     test "no such fn 12" do
       assert catch_error Kwiq1.hello(12)
     end
 
+    @tag :skip
     test "40 seconds" do
       assert Kwiq1.hello(40)
     end
